36dbbbc48bf9f5eaa8025fc7b4469e00b09f0e02
hcl1981 <jan.schulz@rwth-aachen.de>
2021-10-17 17:31:47 +0200

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,60 @@
-#include <Arduino.h>
-
 #include "nRF24L01.h" //NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
-//keine zu neue Version nehmen. z.b. 1.3.5
 #include "RF24.h"
 #include "SPI.h"
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include <Adafruit_ADS1015.h>
 
-Adafruit_ADS1015 ads;
+Adafruit_MPU6050 mpu;
+const float XOFFSET = 0; //0.31;//0.45
+const float YOFFSET = 0;
+const float ZOFFSET = 0;
 
+char message[32] = "Das ist unwichtig..";
+String sMessage;
 RF24 radio(9, 10);
 const uint64_t pipe = 0xE6E6E6E6E6E6;
-const float   multiplier = 3.0F;
-
-char message[10] = "-9999.99";
-String sMessage;
-int16_t results;
 
 void setup(void) {
   delay(100);
   Serial.begin(115200);
-  Wire.begin();
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  //mpu.reset();
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
 
   radio.begin();
   radio.setChannel(1);
-  radio.setPALevel(RF24_PA_MAX);           // If you want to save power use "RF24_PA_MIN" but keep in mind that reduces the module's range
+  radio.setPALevel(RF24_PA_MIN);           // If you want to save power use "RF24_PA_MIN" but keep in mind that reduces the module's range
   radio.setDataRate(RF24_1MBPS);
   radio.setAutoAck(1);                     // Ensure autoACK is enabled
   radio.setRetries(2, 15);
-  //radio.setCRCLength(RF24_CRC_8); //ist das gut/sinnvoll/funktiomiert es?
   radio.openWritingPipe(pipe);
-
-  ads.setGain(GAIN_ONE);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
-  ads.begin();
 }
 
 void loop(void) {
 
-  float   multiplier = 2.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
-  results = ads.readADC_Differential_0_1();
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
 
-  sMessage = String(results * multiplier);
-  sMessage.toCharArray(message, 10);
-  radio.writeFast(message, sizeof(message)); //besser mit timer? oder warten auf millis() +x?
+  sMessage = String(a.acceleration.x-XOFFSET) + ";" + String(a.acceleration.y-YOFFSET) + ";" + String(a.acceleration.z-ZOFFSET);
+  sMessage.toCharArray(message, 32);
+  radio.write(message, sizeof(message));
   Serial.println(message);
-  delay(2);
+  delay(20);
+  //delay(500);
+  //}
+
 }
