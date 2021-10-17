06e72ebc1a9fd45af3d935019ad378521a27b6b3
hcl1981 <jan.schulz@rwth-aachen.de>
2021-10-17 14:19:51 +0200

    1

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include "nRF24L01.h" //NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
+#include "RF24.h"
+#include "SPI.h"
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+const float XOFFSET = 0; //0.31;//0.45
+const float YOFFSET = 0;
+const float ZOFFSET = 0;
+
+char message[32] = "Das ist unwichtig..";
+String sMessage;
+RF24 radio(9, 10);
+const uint64_t pipe = 0xE6E6E6E6E6E6;
+
+void setup(void) {
+  delay(100);
+  Serial.begin(115200);
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  //mpu.reset();
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
+
+  radio.begin();
+  radio.setChannel(1);
+  radio.setPALevel(RF24_PA_MIN);           // If you want to save power use "RF24_PA_MIN" but keep in mind that reduces the module's range
+  radio.setDataRate(RF24_1MBPS);
+  radio.setAutoAck(1);                     // Ensure autoACK is enabled
+  radio.setRetries(2, 15);
+  radio.openWritingPipe(pipe);
+}
+
+void loop(void) {
+
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  sMessage = String(a.acceleration.x-XOFFSET) + ";" + String(a.acceleration.y-YOFFSET) + ";" + String(a.acceleration.z-ZOFFSET);
+  sMessage.toCharArray(message, 32);
+  radio.write(message, sizeof(message));
+  Serial.println(message);
+  delay(20);
+  //delay(500);
+  //}
+
+}
